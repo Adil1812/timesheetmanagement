27bbf99d5ca6c433f53a3b1a6e3c56f7ee2cd881
adil <adil.chougala@hcltech.com>
2025-06-14 15:58:45 +0530

    resolved the error

--- src/main/java/com/timesheetmanagement/mapper/TimesheetEntryMapper.java
+++ src/main/java/com/timesheetmanagement/mapper/TimesheetEntryMapper.java
@@ -1,21 +1,21 @@
-package com.timesheetmanagement.mapper;
-
-import com.timesheetmanagement.entity.TimesheetEntry;
-import com.timesheetmanagement.payload.TimesheetEntryDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-@Mapper(componentModel = "spring")
-public interface TimesheetEntryMapper {
-
-    @Mapping(source = "contractor.id", target = "contractorId")
-    @Mapping(source = "project.projectCode", target = "projectCode")
-    @Mapping(source = "activity.activityCode", target = "activityCode")
-    @Mapping(source = "status", target = "status")
-    TimesheetEntryDTO toDto(TimesheetEntry entity);
-
-    @Mapping(source = "contractorId", target = "contractor.id")
-    @Mapping(source = "projectCode", target = "project.projectCode")
-    @Mapping(source = "activityCode", target = "activity.activityCode")
-    @Mapping(source = "status", target = "status")
-    TimesheetEntry toEntity(TimesheetEntryDTO dto);
-}
+//package com.timesheetmanagement.mapper;
+//
+//import com.timesheetmanagement.entity.TimesheetEntry;
+//import com.timesheetmanagement.payload.TimesheetEntryDTO;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//@Mapper(componentModel = "spring")
+//public interface TimesheetEntryMapper {
+//
+//    @Mapping(source = "contractor.id", target = "contractorId")
+//    @Mapping(source = "project.projectCode", target = "projectCode")
+//    @Mapping(source = "activity.activityCode", target = "activityCode")
+//    @Mapping(source = "status", target = "status")
+//    TimesheetEntryDTO toDto(TimesheetEntry entity);
+//
+//    @Mapping(source = "contractorId", target = "contractor.id")
+//    @Mapping(source = "projectCode", target = "project.projectCode")
+//    @Mapping(source = "activityCode", target = "activity.activityCode")
+//    @Mapping(source = "status", target = "status")
+//    TimesheetEntry toEntity(TimesheetEntryDTO dto);
+//}
--- src/main/java/com/timesheetmanagement/mapper/TimesheetMapper.java
+++ src/main/java/com/timesheetmanagement/mapper/TimesheetMapper.java
@@ -1,16 +1,16 @@
-package com.timesheetmanagement.mapper;
-
-import com.timesheetmanagement.entity.Timesheet;
-import com.timesheetmanagement.payload.TimesheetDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-@Mapper(componentModel = "spring")
-public interface TimesheetMapper {
-    @Mapping(source = "contractor.id", target = "contractorId")
-    @Mapping(source = "approvalStatus", target = "approvalStatus")
-    TimesheetDTO toDto(Timesheet entity);
-
-    @Mapping(source = "contractorId", target = "contractor.id")
-    @Mapping(source = "approvalStatus", target = "approvalStatus")
-    Timesheet toEntity(TimesheetDTO dto);
-}
+//package com.timesheetmanagement.mapper;
+//
+//import com.timesheetmanagement.entity.Timesheet;
+//import com.timesheetmanagement.payload.TimesheetDTO;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//@Mapper(componentModel = "spring")
+//public interface TimesheetMapper {
+//    @Mapping(source = "contractor.id", target = "contractorId")
+//    @Mapping(source = "approvalStatus", target = "approvalStatus")
+//    TimesheetDTO toDto(Timesheet entity);
+//
+//    @Mapping(source = "contractorId", target = "contractor.id")
+//    @Mapping(source = "approvalStatus", target = "approvalStatus")
+//    Timesheet toEntity(TimesheetDTO dto);
+//}
