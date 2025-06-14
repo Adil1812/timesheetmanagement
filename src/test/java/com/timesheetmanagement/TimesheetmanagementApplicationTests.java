cd08f9197dbe1894207fc6b4bc05fb387cdb24fa
adil <adil.chougala@hcltech.com>
2025-06-14 13:59:16 +0530

    project skeleton

---
+++ src/test/java/com/timesheetmanagement/TimesheetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timesheetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
